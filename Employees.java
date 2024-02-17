2881498e7a0a3ecc0ece5001e68a43a421308f53
AlekseyGelezov <152330869+AlekseyGelezov@users.noreply.github.com>
2024-02-17 13:35:35 +0000

    Add files via upload

---
+++ Employees.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employees;
+
+/**
+ *
+ * @author aleksey
+ */
+public class Employees {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String name;
+    String email;
+    int empNum;
+}
+public Employees() {
+    String name = "John Doe";
+  }
+        
+}
+    
+
