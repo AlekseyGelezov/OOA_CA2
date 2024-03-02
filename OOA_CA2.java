f6c69eacb48894ac2126fc1b0bdb445d24c2a8df
AlekseyGelezov <152330869+AlekseyGelezov@users.noreply.github.com>
2024-03-02 11:34:19 +0000

    Add files via upload

---
+++ OOA_CA2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ooa_ca2;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author aleksey
+ * github link: https://github.com/AlekseyGelezov/OOA_CA2.git
+ */
+public class OOA_CA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {   
+    }
+    
+}
